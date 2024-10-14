0a1f4b50baef4280a69abed878bdb73998b1aa4b
ArturoDLopez <alandavidlopezduran053@gmail.com>
2024-10-14 13:41:21 -0600

    Mostrar y leer todos los cursos, paginarlos y mostrar cada currso por separado

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'Cursos'. $curso)
 
 @section('content')
-    <h1>Bienvenido al curso {{$curso}}</h1>
-    
+    <h1>Bienvenido al curso {{$curso->name}}</h1>
+    <a href="{{route("cursos.index")}}">Volver</a>
+    <p><strong>Categoria: </strong>{{$curso->category}}</p>
+    <p>{{$curso->description}}</p>
 @endsection
