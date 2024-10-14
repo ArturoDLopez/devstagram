0d2574f3a188290b182555f95b593790447e31c4
ArturoDLopez <alandavidlopezduran053@gmail.com>
2024-10-14 13:07:40 -0600

    mutadores y accesores (setters and getters)

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+//Agregar un mutador
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -41,4 +44,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //Mutador
+    protected function name(): Attribute{
+        return new Attribute(
+
+            //Accesor
+            get: fn ($value) => ucwords($value),
+
+            set: fn($value) => strtolower($value)
+        );
+    }
 }
